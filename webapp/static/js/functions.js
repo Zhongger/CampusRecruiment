a5d21111c3500c34ffa8eaf9bcb9c0f59196a228
InsistonTan <1763344790@qq.com>
2020-07-05 12:45:43 +0800

    基本完成

---
+++ webapp/static/js/functions.js
@@ -0,0 +1,96 @@
+
+//获取url中的参数
+function getQueryParam(name)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == name){return pair[1];}
+       }
+       return null;
+}
+//
+//检查登陆状态
+function checkLogin()
+{
+        console.log("正在检查登陆...");
+        var loginUser=null;
+        var storage=window.sessionStorage;
+        var type=storage.getItem("type");
+        var username=storage.getItem("username");
+        if(type!=null&&type!=undefined&&username!=null&&username!=undefined){
+                loginUser={"username":username,"type":type};
+        }
+        return loginUser;
+}
+//
+//查看招聘详情
+function seeMsg(msg_id,src)
+{
+        console.log(src+":点击了查看招聘详情");
+        msg_id="123";
+        if(src==1)
+                location.href="pages/seeMsg.html?id="+msg_id;
+        else
+                location.href="seeMsg.html?id="+msg_id;
+}
+
+//创建验证码
+function creatCode(type)
+{
+        if(type=="user"){
+                return makeCode("codecanvas");
+        }
+        else if(type=="com"){
+                return makeCode("codecanvas2");
+        }
+}
+//
+//创建验证码
+function makeCode(id)
+{
+	showCode="";
+	//document.getElementById('codecanvas')
+	var canvas=document.getElementById(id);
+	if(canvas!=null)
+	{
+		var width=canvas.clientWidth;
+		var height=canvas.clientHeight;
+		var context=canvas.getContext("2d");
+		canvas.width=width;
+		canvas.height=height;
+		var sCode="q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,1,2,3,4,5,6,7,8,9,0";
+		var aCode=sCode.split(",");
+		var codeLength=aCode.length;
+
+		for(var i=0;i<=3;i++)
+		{
+			var index=Math.floor(Math.random()*codeLength);
+			showCode+=aCode[index];
+		}
+		context.translate(10,25);
+		context.font="bold 23px 微软雅黑";
+		context.fillStyle=randomColor();
+		context.fillText(showCode,0,0);
+
+		for(var i=0;i<=10;i++)
+		{
+			context.translate(-5,-5);
+			context.strokeStyle=randomColor();
+			context.beginPath();
+			context.moveTo(Math.random()*width,Math.random()*height);
+			context.lineTo(Math.random()*width,Math.random()*height);
+			context.stroke();
+                }
+                return showCode;
+	}
+}
+//随机颜色
+function randomColor()
+{
+	var r=Math.floor(Math.random()*256);
+	var g=Math.floor(Math.random()*256);
+	var b=Math.floor(Math.random()*256);
+	return "rgb("+r+","+g+","+b+")";
+}
---
+++ webapp/static/js/index.js
@@ -0,0 +1,97 @@
+var user=null;
+//
+$(document).ready(function(){
+	//alert("1100");
+	user=checkLogin();
+	getNewMsg();
+	if(user!=null){
+		if(user.type!=undefined&&user.type!=null){
+			if(user.type=="user"){
+				$("#username").html(user.username);
+				$("#myFindJob").css("display","inline");
+			}
+			else if(user.type=="com"){
+				$("#username").html(user.username);
+				$("#publishJob").css("display","inline");
+				$("#myPublishJob").css("display","inline");
+			}
+		}
+	}
+	else{
+		$("#login").css("display","inline");
+		$("#register").css("display","inline");
+	}
+	//
+	//var data=[{"companyName":"虎牙","id":123,"salary":"5k/month","deadLine":"2020-07-23","applyPosition":"java后台开发"}];
+	//showMsg(data);
+});
+
+//搜索按钮
+function search()
+{
+	console.log("点击了搜索");
+	var key=$("#search-input").val();
+	if(key==""){
+		alert("搜索内容为空，请检查！");
+		return;
+	}
+	var url="/campus/user/recruitInfo";
+	var ajaxData={"operate":"condition","condition":key};
+	$.ajax({
+        url:url,
+		type:"post",
+		data:ajaxData,
+        dataType:"JSON",
+        success:function(res){
+			console.log(res);
+			if(res.flag=="success"){
+				$("#showMsg").html("");
+				$("#showMsg").append("<div style='text-align: center;color:#00BFBF;'>搜索结果:"+res.data.length+"</div>");
+				$("#showMsg").append("<hr>");
+				showMsg(res.data);
+			}
+        },
+        error:function(res){
+			alert("连接服务器失败！");
+            console.log(res);
+        }
+    });
+}
+//获取最新招聘信息
+function getNewMsg()
+{
+	console.log("正在获取最新招聘信息...");
+	var url="/campus/user/recruitInfo";
+	var ajaxData={"operate":"all"};
+	$.ajax({
+        url:url,
+		type:"post",
+		data:ajaxData,
+        dataType:"JSON",
+        success:function(res){
+			if(res.flag=="success"){
+				showMsg(res.data);
+			}
+        },
+        error:function(res){
+            console.log(res);
+        }
+    });
+	
+}
+//显示招聘简介
+function showMsg(data){
+	var len=data.length;
+	for(var i=0;i<len;i++){
+			var temp=data[i];
+			var str="<div class='msg-body' onclick='seeMsg("+temp.id+",1)'>"
+					+"<div style='font-size: 18px;color: rgb(0,255,255);'>"+temp.companyName+"</div>"
+					+"<div style='font-size: 12px;'>截止时间："+temp.deadLine+"</div>"
+					+"<div style='margin-top: 5px;'>招聘职位："+temp.applyPosition+"</div>"
+					+"<div style='margin-top: 5px;'>薪资："+temp.salary+"</div>"
+					+"<div class='text_seeMore'>点击查看详情</div>"
+					+"</div>"
+			$("#showMsg").append(str);
+	}
+}
+
